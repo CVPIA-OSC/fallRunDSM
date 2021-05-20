10fcbc85287bb4ce49b223abf6223448bee42c30
erodriguez <erodriguez@flowwest.com>
2021-05-20 09:41:35 -0700

    adds: scratch work for using "modes" and caches labels data for usage across the entire package

--- data-raw/cache-data.R
+++ data-raw/cache-data.R
@@ -86,3 +86,36 @@ diversity_group <- original_groups$DiversityGroup
 names(diversity_group) <- original_groups$watershed
 usethis::use_data(diversity_group, overwrite = TRUE)
 
+
+
+watershed_labels <- c("Upper Sacramento River", "Antelope Creek", "Battle Creek",
+                      "Bear Creek", "Big Chico Creek", "Butte Creek", "Clear Creek",
+                      "Cottonwood Creek", "Cow Creek", "Deer Creek", "Elder Creek",
+                      "Mill Creek", "Paynes Creek", "Stony Creek", "Thomes Creek",
+                      "Upper-mid Sacramento River", "Sutter Bypass", "Bear River",
+                      "Feather River", "Yuba River", "Lower-mid Sacramento River",
+                      "Yolo Bypass", "American River", "Lower Sacramento River", "Calaveras River",
+                      "Cosumnes River", "Mokelumne River", "Merced River", "Stanislaus River",
+                      "Tuolumne River", "San Joaquin River")
+
+usethis::use_data(watershed_labels)
+
+size_class_labels <- c('s', 'm', 'l', 'vl')
+
+usethis::use_data(size_class_labels)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
