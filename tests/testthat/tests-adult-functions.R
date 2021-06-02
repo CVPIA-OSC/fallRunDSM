b8c2d7b77e95120e127836aadeca50eb97297f39
ErinCain <ecain@flowwest.com>
2021-06-02 10:39:12 -0700

    Adds addional tests for adult functions.

--- tests/testthat/tests-adult-functions.R
+++ tests/testthat/tests-adult-functions.R
@@ -111,8 +111,141 @@ test_that('The egg_to_fry survival function returns the expected values for year
 })
 
 
+# Test get_spawning_adults
+adults <- structure(c(22012, 72, 12626, 12, 12, 885, 8555, 1251, 1649,
+                      569, 12, 1332, 51, 12, 12, 0, 0, 12, 52408, 7184, 0, 0, 24959,
+                      0, 12, 499, 4514, 2145, 5405, 984, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                      0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                      0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                      0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                      0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                      0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                      0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                      0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                      0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                      0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                      0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                      0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                      0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                      0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                      0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                      0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                      0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                      0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                      0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                      0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                      0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                      0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                      0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                      0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                      0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                      0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                      0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                      0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                      0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                      0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                      0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                      0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                      0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                      0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                      0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                      0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                      0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                      0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                      0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                      0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                      0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                      0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                      0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                      0, 0, 0, 0, 0, 0, 0, 0, 0), .Dim = 31:30, .Dimnames = list(c("Upper Sacramento River",
+                                                                                   "Antelope Creek", "Battle Creek", "Bear Creek", "Big Chico Creek",
+                                                                                   "Butte Creek", "Clear Creek", "Cottonwood Creek", "Cow Creek",
+                                                                                   "Deer Creek", "Elder Creek", "Mill Creek", "Paynes Creek", "Stony Creek",
+                                                                                   "Thomes Creek", "Upper-mid Sacramento River", "Sutter Bypass",
+                                                                                   "Bear River", "Feather River", "Yuba River", "Lower-mid Sacramento River",
+                                                                                   "Yolo Bypass", "American River", "Lower Sacramento River", "Calaveras River",
+                                                                                   "Cosumnes River", "Mokelumne River", "Merced River", "Stanislaus River",
+                                                                                   "Tuolumne River", "San Joaquin River"), NULL))
+hatch_adults <- c(`Upper Sacramento River` = 6926L, `Antelope Creek` = 13L, `Battle Creek` = 16007L,
+                  `Bear Creek` = 15L, `Big Chico Creek` = 18L, `Butte Creek` = 80L,
+                  `Clear Creek` = 1082L, `Cottonwood Creek` = 914L, `Cow Creek` = 274L,
+                  `Deer Creek` = 132L, `Elder Creek` = 15L, `Mill Creek` = 66L,
+                  `Paynes Creek` = 25L, `Stony Creek` = 16L, `Thomes Creek` = 13L,
+                  `Upper-mid Sacramento River` = 0L, `Sutter Bypass` = 0L, `Bear River` = 21L,
+                  `Feather River` = 50417L, `Yuba River` = 8136L, `Lower-mid Sacramento River` = 0L,
+                  `Yolo Bypass` = 0L, `American River` = 14083L, `Lower Sacramento River` = 0L,
+                  `Calaveras River` = 28L, `Cosumnes River` = 14L, `Mokelumne River` = 2926L,
+                  `Merced River` = 1402L, `Stanislaus River` = 1506L, `Tuolumne River` = 475L,
+                  `San Joaquin River` = 0L)
+seeds <- NULL
+
+expected_spawners <- list(init_adults = c(22012, 72, 3171, 12, 12, 885, 8555, 1251,
+                                          1649, 569, 12, 1332, 51, 12, 12, 0, 0, 12, 39562, 7184, 0, 0,
+                                          19284, 0, 12, 499, 2747, 1526, 5405, 984, 0),
+                          proportion_natural = c(`Upper Sacramento River` = 0.63,
+                                                 `Antelope Creek` = 0.8, `Battle Creek` = 0.1, `Bear Creek` = 0.62031746031746,
+                                                 `Big Chico Creek` = 0.8, `Butte Creek` = 0.885, `Clear Creek` = 0.7775,
+                                                 `Cottonwood Creek` = 0.6475, `Cow Creek` = 0.8, `Deer Creek` = 0.84,
+                                                 `Elder Creek` = 0.62031746031746, `Mill Creek` = 0.8475, `Paynes Creek` = 0.635,
+                                                 `Stony Creek` = 0.62031746031746, `Thomes Creek` = 0.62031746031746,
+                                                 `Upper-mid Sacramento River` = 0.62031746031746, `Sutter Bypass` = 0.62031746031746,
+                                                 `Bear River` = 1, `Feather River` = 0.145, `Yuba River` = 0.46,
+                                                 `Lower-mid Sacramento River` = 0.62031746031746, `Yolo Bypass` = 0.62031746031746,
+                                                 `American River` = 0.428333333333333, `Lower Sacramento River` = 0.62031746031746,
+                                                 `Calaveras River` = 0.923333333333333, `Cosumnes River` = 0.98,
+                                                 `Mokelumne River` = 0.2425, `Merced River` = 0.255, `Stanislaus River` = 0.295,
+                                                 `Tuolumne River` = 0.535, `San Joaquin River` = 0.62031746031746),
+                          natural_adults = c(22012, 72, 12626, 12, 12, 885, 8555, 1251,
+                                             1649, 569, 12, 1332, 51, 12, 12, 0, 0, 12, 52408, 7184, 0, 0,
+                                             24959, 0, 12, 499, 4514, 2145, 5405, 984, 0),
+                          init_adults_by_month = structure(c(4867L,
+                                                             17L, 741L, 3L, 4L, 195L, 1839L, 277L, 331L, 139L, 1L, 287L, 15L,
+                                                             1L, 3L, 0L, 0L, 1L, 8886L, 1611L, 0L, 0L, 4280L, 0L, 1L, 118L,
+                                                             574L, 353L, 1223L, 227L, 0L, 12175L, 38L, 1727L, 6L, 7L, 467L,
+                                                             4777L, 724L, 971L, 328L, 6L, 764L, 31L, 6L, 6L, 0L, 0L, 8L, 21864L,
+                                                             4016L, 0L, 0L, 10714L, 0L, 7L, 281L, 1572L, 841L, 3053L, 516L,
+                                                             0L, 4970L, 17L, 703L, 3L, 1L, 223L, 1939L, 250L, 347L, 102L,
+                                                             5L, 281L, 5L, 5L, 3L, 0L, 0L, 3L, 8812L, 1557L, 0L, 0L, 4290L,
+                                                             0L, 4L, 100L, 601L, 332L, 1129L, 241L, 0L), .Dim = c(31L, 3L)))
+test_that("Get spawning adults returns the expected values", {
+
+  set.seed(2021)
+  spawning_adults <- get_spawning_adults(year = year, adults = adults, hatch_adults = hatch_adults, seeds = seeds)
+  expect_equal(spawning_adults, expected_spawners)
+})
 
 
+# Tests spawn success function
+init_adults <- expected_spawners$init_adults
+min_spawn_habitat <- apply(test_data$spawning_habitat[ , 10:12, year], 1, min)
 
+expected_juveniles <- structure(c(23562632.6818975, 95877.4873630614, 400486.276147004,
+                                  14696.4466670538, 15466.5250672724, 1230801.9983072, 11392333.5307564,
+                                  1505953.40992132, 2237111.18034545, 770950.13685259, 15511.9369082167,
+                                  1660902.32553158, 62818.1321951005, 14307.5895999286, 14822.7873816012,
+                                  0, 0, 15335.6115608913, 26878201.5840594, 9066230.947591, 0,
+                                  0, 23435759.0578475, 0, 14870.5408311819, 651645.917994897, 3098441.89405339,
+                                  1235205.31252439, 6286476.48377482, 1223531.384084, 0, 0, 0,
+                                  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                                  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                                  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                                  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                                  0, 0, 0, 0, 0, 0, 0), .Dim = c(31L, 4L), .Dimnames = list(c("Upper Sacramento River",
+                                                                                              "Antelope Creek", "Battle Creek", "Bear Creek", "Big Chico Creek",
+                                                                                              "Butte Creek", "Clear Creek", "Cottonwood Creek", "Cow Creek",
+                                                                                              "Deer Creek", "Elder Creek", "Mill Creek", "Paynes Creek", "Stony Creek",
+                                                                                              "Thomes Creek", "Upper-mid Sacramento River", "Sutter Bypass",
+                                                                                              "Bear River", "Feather River", "Yuba River", "Lower-mid Sacramento River",
+                                                                                              "Yolo Bypass", "American River", "Lower Sacramento River", "Calaveras River",
+                                                                                              "Cosumnes River", "Mokelumne River", "Merced River", "Stanislaus River",
+                                                                                              "Tuolumne River", "San Joaquin River"), c("fry", "", "", "")))
 
+test_that("spawn success function returns the expected value", {
+  juveniles <- spawn_success(escapement = init_adults,
+                             adult_prespawn_survival = expected_prespawn_surv,
+                             egg_to_fry_survival = expected_egg_surv,
+                             prob_scour = test_data$prob_nest_scoured,
+                             spawn_habitat = min_spawn_habitat)
+  expect_equal(juveniles, expected_juveniles)
+})
 
