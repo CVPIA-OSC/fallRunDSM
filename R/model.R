4321099d947da396c933919508a27706eacd4084
erodriguez <erodriguez@flowwest.com>
2021-06-22 13:10:12 -0700

    adds hatchery_allocation to the list of model params

--- R/model.R
+++ R/model.R
@@ -76,7 +76,7 @@ fall_run_model <- function(scenario = NULL, mode = c("seed", "simulate", "calibr
     annual_migrants <- matrix(0, nrow = 31, ncol = 4, dimnames = list(fallRunDSM::watershed_labels, fallRunDSM::size_class_labels))
     avg_ocean_transition_month <- ocean_transition_month() # 2
 
-    hatch_adults <- rmultinom(1, size = round(runif(1, 83097.01,532203.1)), prob = hatchery_allocation)[ , 1]
+    hatch_adults <- rmultinom(1, size = round(runif(1, 83097.01,532203.1)), prob = ..params$hatchery_allocation)[ , 1]
     spawners <- get_spawning_adults(year, round(adults), hatch_adults, mode = mode,
                                     prop_flow_natal = ..params$prop_flow_natal,
                                     south_delta_routed_watersheds = ..params$south_delta_routed_watersheds,
