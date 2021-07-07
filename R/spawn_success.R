26c3a821b5326e31efd9b13255334ff83ac6e301
sadie <sgill@flowwest.com>
2021-07-07 11:45:14 -0700

    gets rid of ifelse on spawn success

--- R/spawn_success.R
+++ R/spawn_success.R
@@ -19,16 +19,25 @@ spawn_success <- function(escapement, adult_prespawn_survival, egg_to_fry_surviv
 
   capacity <- spawn_habitat / redd_size
 
-  spawner_potential <- ifelse(max(escapement) <= 1000000000,
-                              rbinom(31, round(escapement), (adult_prespawn_survival * sex_ratio)),
-                              round(escapement * adult_prespawn_survival * sex_ratio))
+  spawner_potential <- if(max(escapement) <= 1000000000) {
+    rbinom(31, round(escapement), (adult_prespawn_survival * sex_ratio))
+  } else {
+    round(escapement * adult_prespawn_survival * sex_ratio)
+  }
 
-  spawners <- ifelse(spawner_potential > capacity, capacity, spawner_potential)
+  spawners <- if(spawner_potential > capacity) {
+    capacity
+  } else {
+    spawner_potential
+  }
+
   fry <- spawners * (1 - prob_scour) * fecundity * egg_to_fry_survival
 
-  fry <- ifelse(max(fry) <= 1000000000,
-                pmax(round(rnorm(31, fry, (sqrt(fry) / 2))), 0),
-                round(fry))
+  fry <- if(max(fry) <= 1000000000) {
+    pmax(round(rnorm(31, fry, (sqrt(fry) / 2))), 0)
+  } else {
+    round(fry)
+  }
 
   zeros <- matrix(0, nrow = length(escapement), ncol = 3)
   cbind(fry, zeros)
