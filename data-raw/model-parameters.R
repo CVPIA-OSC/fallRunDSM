3157f3d606b125b1339a7a504f8b3318dc869a8b
ErinCain <ecain@flowwest.com>
2021-07-13 14:00:05 -0700

    Add and remove comments from params list

--- data-raw/model-parameters.R
+++ data-raw/model-parameters.R
@@ -3,7 +3,7 @@ library(tidyverse)
 # 2019 Calibration Parameters ------------
 params <- list(
 
-  # items from dsm package
+  # Data from fallRunDSM cache-data (values vary by run)
   hatchery_allocation = fallRunDSM::hatchery_allocation,
   natural_adult_removal_rate = fallRunDSM::natural_adult_removal_rate,
   proportion_hatchery = fallRunDSM::proportion_hatchery,
@@ -17,7 +17,7 @@ params <- list(
   adult_harvest_rate = fallRunDSM::adult_harvest_rate,
   diversity_group = fallRunDSM::diversity_group,
 
-  # adults
+  # Coefficients for adult submodules
   .adult_stray_intercept = 3,
   .adult_stray_wild = -5.5,
   .adult_stray_natal_flow = -1.99,
@@ -26,12 +26,14 @@ params <- list(
   .adult_stray_prop_delta_trans = 2.89,
   .adult_en_route_migratory_temp = -0.26,
   .adult_en_route_bypass_overtopped = -0.019,
-  .adult_en_route_adult_harvest_rate = fallRunDSM::adult_harvest_rate,
+  .adult_en_route_adult_harvest_rate = fallRunDSM::adult_harvest_rate, # varies by run
   .adult_prespawn_deg_day = -0.000669526,
-  # growth
+
+  # Ocean entry success coefficient and variable
   ocean_entry_success_length = c(-0.0897309864, -0.0709704348, -0.0208590732, 0.0732620916),
   .ocean_entry_success_months = 0.35,
-  # rear
+
+  # Pulse movement coefficients and variables
   .pulse_movement_intercept = -7.70744,
   .pulse_movement_proportion_pulse = 0.26579,
   pulse_movement_medium = 1.66845,
@@ -41,13 +43,43 @@ params <- list(
   .pulse_movement_large_pulse = -0.44778,
   .pulse_movement_very_large_pulse = 0.329,
 
+  # Spawn success variables
   spawn_success_sex_ratio = 0.5,
   spawn_success_redd_size = 9.29,
   spawn_success_fecundity = 5522,
 
+  # Egg to fry survival coefficients
   .surv_egg_to_fry_proportion_natural = 0.533,
   .surv_egg_to_fry_scour = -0.655,
 
+  # Juvenile rearing survival coefficients and variables
+  .surv_juv_rear_avg_temp_thresh = -0.717,
+  .surv_juv_rear_high_predation = -0.122,
+  .surv_juv_rear_stranded = -1.939,
+  surv_juv_rear_medium = 1.48,
+  surv_juv_rear_large = 2.223,
+  surv_juv_rear_floodplain = 0.47,
+  min_survival_rate = 0.0001,
+
+  # Juvenile bypass survival coefficients and variables
+  .surv_juv_bypass_avg_temp_thresh = -0.717,
+  .surv_juv_bypass_high_predation = -0.122,
+  surv_juv_bypass_medium = 1.48,
+  surv_juv_bypass_large = 2.223,
+  surv_juv_bypass_floodplain = 0.47,
+
+  # Juvenile delta survival coefficients and variables
+  .surv_juv_delta_avg_temp_thresh = -0.717,
+  .surv_juv_delta_high_predation = -0.122,
+  .surv_juv_delta_prop_diverted = -3.51,
+  surv_juv_delta_medium = 1.48,
+  surv_juv_delta_large = 2.223,
+
+  # San joaquin outmigration variables
+  surv_juv_outmigration_san_joaquin_medium = 1.48,
+  surv_juv_outmigration_san_joaquin_large = 2.223,
+
+  # Sac delta outmigration coefficients and variables
   .surv_juv_outmigration_sac_delta_intercept_one = -3.5,
   .surv_juv_outmigration_sac_delta_intercept_two =  0.3,
   .surv_juv_outmigration_sac_delta_intercept_three = -3.5,
@@ -57,28 +89,7 @@ params <- list(
   surv_juv_outmigration_sac_delta_medium = 1.48,
   surv_juv_outmigration_sac_delta_large = 2.223,
 
-  # survival
-  .surv_juv_rear_avg_temp_thresh = -0.717,
-  .surv_juv_rear_high_predation = -0.122,
-  .surv_juv_rear_stranded = -1.939,
-  surv_juv_rear_medium = 1.48,
-  surv_juv_rear_large = 2.223,
-  surv_juv_rear_floodplain = 0.47,
-  .surv_juv_bypass_avg_temp_thresh = -0.717,
-  .surv_juv_bypass_high_predation = -0.122,
-  surv_juv_bypass_medium = 1.48,
-  surv_juv_bypass_large = 2.223,
-  surv_juv_bypass_floodplain = 0.47,
-  .surv_juv_delta_avg_temp_thresh = -0.717,
-  .surv_juv_delta_high_predation = -0.122,
-  .surv_juv_delta_prop_diverted = -3.51,
-  surv_juv_delta_medium = 1.48,
-  surv_juv_delta_large = 2.223,
-  surv_juv_outmigration_san_joaquin_medium = 1.48,
-  surv_juv_outmigration_san_joaquin_large = 2.223,
-  min_survival_rate = 0.0001,
-
-  ### load baseline data
+  ## Variable from load baseline data
   # DSMflow variables -----
   freeport_flows = DSMflow::freeport_flow,
   vernalis_flows = DSMflow::vernalis_flow,
@@ -109,9 +120,9 @@ params <- list(
 
   # DSMhabitat variables -----
   spawning_habitat = DSMhabitat::fr_spawn,
-  inchannel_habitat_fry = DSMhabitat::fr_fry,
-  inchannel_habitat_juvenile = DSMhabitat::fr_juv,
-  floodplain_habitat = DSMhabitat::fr_fp,
+  inchannel_habitat_fry = DSMhabitat::fr_fry, # vary by run
+  inchannel_habitat_juvenile = DSMhabitat::fr_juv, # vary by run
+  floodplain_habitat = DSMhabitat::fr_fp, # vary by run
   weeks_flooded = DSMhabitat::weeks_flooded,
   delta_habitat = DSMhabitat::delta_habitat,
   sutter_habitat = DSMhabitat::sutter_habitat,
@@ -127,7 +138,7 @@ params <- list(
   prob_strand_late = DSMhabitat::prob_strand_late,
   prob_nest_scoured = DSMhabitat::prob_nest_scoured,
 
-
+  # Calibration Variables (vary by run)
   ..surv_adult_enroute_int = 3,
   ..surv_adult_prespawn_int = 3,
   ..surv_egg_to_fry_int = 0.041,
