fa7e4cc62df1345c0b7914e3a0de69516f02f9ee
ErinCain <ecain@flowwest.com>
2021-07-16 08:46:26 -0700

    Fix remaining tests after params refactor

--- tests/testthat/tests-rear-and-growth-functions.R
+++ tests/testthat/tests-rear-and-growth-functions.R
@@ -12,11 +12,14 @@ growth <- structure(c(0.0030986844080139, 0, 0, 0, 0.996809996864753, 0.50831147
                       0, 0, 1.27897692436818e-13, 0.186181640595347, 1), .Dim = c(4L,
                                                                                   4L), .Dimnames = list(c("s", "m", "l", "vl"), c("s", "m", "l",  "vl")))
 # Tests rear fucntion
-expected_rearing_output <- structure(c(0, 33215.9371435199, 0, 0, 10685172.7508171, 0, 0,
-                                       978.879002963796, 0, 0, 0, 0), .Dim = 3:4, .Dimnames = list(NULL,
-                                                                                                   c("s", "m", "l", "vl")))
+expected_rearing_output <- structure(c(0, 33217, 0, 0, 10685535, 0, 0, 979, 0, 0, 0, 0),
+                                     .Dim = 3:4,
+                                     .Dimnames = list(NULL, c("s", "m", "l", "vl")))
+
 test_that('The rearing function returns the expected values for year 1', {
-  expect_equal(rear(juveniles = juveniles, survival_rate = survival_rate, growth = growth),
+  set.seed(2021)
+  rearing <- rear(juveniles = juveniles, survival_rate = survival_rate, growth = growth)
+  expect_equal(rearing,
                expected_rearing_output)
 })
 
