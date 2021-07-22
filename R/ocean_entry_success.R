0df5d0bdb5a507fd9a1fb5400ae99b55df2cac83
ErinCain <ecain@flowwest.com>
2021-07-22 13:56:02 -0700

    replaces all fal with winter in ocean entry success

--- R/ocean_entry_success.R
+++ R/ocean_entry_success.R
@@ -10,9 +10,9 @@
 #' @source IP-117068
 #' @export
 ocean_entry_success <- function(migrants, month, avg_ocean_transition_month,
-                                .ocean_entry_success_length = fallRunDSM::params$.ocean_entry_success_length,
-                                ..ocean_entry_success_int = fallRunDSM::params$..ocean_entry_success_int,
-                                .ocean_entry_success_months = fallRunDSM::params$.ocean_entry_success_months){
+                                .ocean_entry_success_length = winterRunDSM::params$.ocean_entry_success_length,
+                                ..ocean_entry_success_int = winterRunDSM::params$..ocean_entry_success_int,
+                                .ocean_entry_success_months = winterRunDSM::params$.ocean_entry_success_months){
 
   month_since <- ifelse(month <= avg_ocean_transition_month, 0, max(1, month - avg_ocean_transition_month))
 
