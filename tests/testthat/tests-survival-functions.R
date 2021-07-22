425a11eba89dc8025afa1989e8d9d22787579335
ErinCain <ecain@flowwest.com>
2021-07-22 09:54:15 -0700

    Fixes test for surv juv rear function

--- tests/testthat/tests-survival-functions.R
+++ tests/testthat/tests-survival-functions.R
@@ -2,7 +2,6 @@ library(testthat)
 library(fallRunDSM)
 # tests for survival functions
 # Lists inputs to use in testing
-test_data <- fallRunDSM::load_baseline_data()
 year <- 1
 month <- 9
 aveT20 <- c(0L, 0L, 0L, 1L, 1L, 0L, 0L, 0L, 1L, 0L, 0L, 1L, 0L, 1L, 0L,
@@ -19,24 +18,23 @@ betas <- as.matrix(survival_betas[, 3:16])
 bp_survival_betas <- as.matrix(survival_betas[c(17, 22), c(3, 4, 5, 13, 14, 15)])
 
 # Tests surv_juv_rear survival function
-expected_surv_juv_rear <- list(inchannel = cbind(s = 0.313393973985376,
-                                                 m = 0.667233816948951,
-                                                 l = 0.808253327118087,
-                                                 vl = 1),
-                               floodplain = structure(c(0.520325456943001, 0.802578450626017,
-                                                        0.89145699095444, 1),
+expected_surv_juv_rear <- list(inchannel = structure(c(0.373145308705724, 0.723372636446464,
+                                                       0.846089404751765, 1),
+                                                     .Dim = c(1L, 4L),
+                                                     .Dimnames = list(NULL, c("s", "m", "l", "vl"))),
+                               floodplain = structure(c(0.567765169512671, 0.835659795386719, 0.912083864792188, 1),
                                                       .Dim = c(1L, 4L),
-                                                      .Dimnames = list("Upper Sacramento River",
-                                                                       c("s", "m", "l", "vl"))))
+                                                      .Dimnames = list( "Upper Sacramento River", c("s", "m", "l", "vl"))))
+
 test_that('The surv_juv_rear function returns the expected values for year 1 month 9 watershed 1', {
   expect_equal(surv_juv_rear(max_temp_thresh = maxT25[1],
                              avg_temp_thresh = aveT20[1],
                              high_predation = high_predation[1],
-                             contact_points = test_data$contact_points[1],
-                             prop_diversions = test_data$proportion_diverted[1],
-                             total_diversions = test_data$total_diverted[1],
+                             contact_points = fallRunDSM::params$contact_points[1],
+                             prop_diversions = fallRunDSM::params$proportion_diverted[1],
+                             total_diversions = fallRunDSM::params$total_diverted[1],
                              stranded = ws_strand[1],
-                             weeks_flooded = test_data$weeks_flooded[, month, year][1],
+                             weeks_flooded = fallRunDSM::params$weeks_flooded[, month, year][1],
                              ..surv_juv_rear_int = betas[1, 1],
                              ..surv_juv_rear_contact_points = -0.0067662,
                              ..surv_juv_rear_prop_diversions = -0.1755,
