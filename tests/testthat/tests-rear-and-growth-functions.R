ef6eee547c39cc8c50193661ea7b4b11247d8b30
ErinCain <ecain@flowwest.com>
2021-07-22 14:50:21 -0700

    Adds test for growth function

--- tests/testthat/tests-rear-and-growth-functions.R
+++ tests/testthat/tests-rear-and-growth-functions.R
@@ -24,3 +24,16 @@ test_that('The rearing function returns the expected values for year 1', {
   expect_equal(rearing,
                expected_rearing_output)
 })
+
+# Tests growth functions
+expected_growth <- structure(c(0.0030986844080139, 0, 0, 0, 0.996809996864753, 0.508311476301429,
+                               0, 0, 9.13187272335581e-05, 0.491688523698443, 0.813818359404653,
+                               0, 0, 1.27897692436818e-13, 0.186181640595347, 1), .Dim = c(4L,
+                                                                                           4L), .Dimnames = list(c("s", "m", "l", "vl"), c("s", "m", "l",
+                                                                                                                                           "vl")))
+test_that('The growth() function returns the expected value', {
+  set.seed(2021)
+  growth <- growth()
+  expect_equal(growth, expected_growth)
+})
+
