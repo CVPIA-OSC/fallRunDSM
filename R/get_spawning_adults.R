663df810885cecbbb78b1d8d7346ca5a2008361a
ErinCain <ecain@flowwest.com>
2021-07-22 14:24:21 -0700

    Fix get spawning adult logic to use correct months and remove straying for winter run:

--- R/get_spawning_adults.R
+++ R/get_spawning_adults.R
@@ -51,7 +51,7 @@ get_spawning_adults <- function(year, adults, hatch_adults, mode,
       rmultinom(1, adults[watershed, adult_index], month_return_proportions)
     }))
 
-    natural_adults_by_month <- sapply(1:3, function(month) {
+    natural_adults_by_month <- sapply(1:4, function(month) {
       rbinom(n = 31,
              size = round(adults_by_month[, month]),
              prob = 1 - winterRunDSM::params$natural_adult_removal_rate)
@@ -72,50 +72,50 @@ get_spawning_adults <- function(year, adults, hatch_adults, mode,
       rmultinom(1, hatch_adults[watershed], month_return_proportions)
     }))
 
-    #TODO random variable
-    stray_props <- sapply(10:12, function(month) {
-      adult_stray(wild = 1,
-                  natal_flow = prop_flow_natal[ , year],
-                  south_delta_watershed = south_delta_routed_watersheds,
-                  cross_channel_gates_closed = cc_gates_days_closed[month],
-                  .intercept = .adult_stray_intercept,
-                  .wild = .adult_stray_wild,
-                  .natal_flow = .adult_stray_natal_flow,
-                  .cross_channel_gates_closed = .adult_stray_cross_channel_gates_closed,
-                  .prop_bay_trans = .adult_stray_prop_bay_trans,
-                  .prop_delta_trans = .adult_stray_prop_delta_trans)
-    })
-
-    straying_adults <- sapply(1:3, function(month) {
-      rbinom(n = 31, adults_by_month[, month], stray_props[, month])
-    })
-
-    south_delta_routed_adults <- round(colSums(straying_adults * south_delta_routed_watersheds))
-    south_delta_stray_adults <- sapply(1:3, function(month) {
-      as.vector(rmultinom(1, south_delta_routed_adults[month], winterRunDSM::params$cross_channel_stray_rate))
-    })
-
-    remaining_stray_adults <- round(colSums(straying_adults * (1 - south_delta_routed_watersheds)))
-    stray_adults <- sapply(1:3, function(month) {
-      as.vector(rmultinom(1, remaining_stray_adults[month], winterRunDSM::params$stray_rate))
-    })
-
-
-    adults_after_stray <- adults_by_month - straying_adults + south_delta_stray_adults + stray_adults
+    # No stray in winter (at least according to WR OG model)
+    # stray_props <- sapply(10:12, function(month) {
+    #   adult_stray(wild = 1,
+    #               natal_flow = prop_flow_natal[ , year],
+    #               south_delta_watershed = south_delta_routed_watersheds,
+    #               cross_channel_gates_closed = cc_gates_days_closed[month],
+    #               .intercept = .adult_stray_intercept,
+    #               .wild = .adult_stray_wild,
+    #               .natal_flow = .adult_stray_natal_flow,
+    #               .cross_channel_gates_closed = .adult_stray_cross_channel_gates_closed,
+    #               .prop_bay_trans = .adult_stray_prop_bay_trans,
+    #               .prop_delta_trans = .adult_stray_prop_delta_trans)
+    # })
+    #
+    # straying_adults <- sapply(1:3, function(month) {
+    #   rbinom(n = 31, adults_by_month[, month], stray_props[, month])
+    # })
+    #
+    # south_delta_routed_adults <- round(colSums(straying_adults * south_delta_routed_watersheds))
+    # south_delta_stray_adults <- sapply(1:3, function(month) {
+    #   as.vector(rmultinom(1, south_delta_routed_adults[month], winterRunDSM::params$cross_channel_stray_rate))
+    # })
+    #
+    # remaining_stray_adults <- round(colSums(straying_adults * (1 - south_delta_routed_watersheds)))
+    # stray_adults <- sapply(1:3, function(month) {
+    #   as.vector(rmultinom(1, remaining_stray_adults[month], winterRunDSM::params$stray_rate))
+    # })
+    #
+    #
+    # adults_after_stray <- adults_by_month - straying_adults + south_delta_stray_adults + stray_adults
 
     # are tisdale or yolo bypasses overtopped?
     # for all years and months 10-12 there is always at least one true
 
-    bypass_is_overtopped <- sapply(10:12, function(month) {
+    bypass_is_overtopped <- sapply(1:4, function(month) {
 
       tis <- gates_overtopped[month, year, 1] * tisdale_bypass_watershed
       yolo <- gates_overtopped[month, year, 2] * yolo_bypass_watershed
       as.logical(tis + yolo)
     })
 
-    en_route_temps <- migratory_temperature_proportion_over_20[, 10:12]
+    en_route_temps <- migratory_temperature_proportion_over_20[, 1:4]
 
-    adult_en_route_surv <- sapply(1:3, function(month) {
+    adult_en_route_surv <- sapply(1:4, function(month) {
       adult_en_route_surv <- surv_adult_enroute(migratory_temp = en_route_temps[,month],
                                                 bypass_overtopped = bypass_is_overtopped[,month],
                                                 adult_harvest = .adult_en_route_adult_harvest_rate,
@@ -125,15 +125,15 @@ get_spawning_adults <- function(year, adults, hatch_adults, mode,
     })
 
 
-    adults_survived_to_spawning <- sapply(1:3, function(month) {
-      rbinom(31, round(adults_after_stray[, month]), adult_en_route_surv[, month])
+    adults_survived_to_spawning <- sapply(1:4, function(month) {
+      rbinom(31, round(adults_by_month[, month]), adult_en_route_surv[, month])
     })
 
-    surviving_natural_adults_by_month <- sapply(1:3, function(month) {
+    surviving_natural_adults_by_month <- sapply(1:4, function(month) {
       rbinom(31, round(adults_survived_to_spawning[, month]), (1 - winterRunDSM::params$natural_adult_removal_rate))
     })
 
-    surviving_hatchery_adults_by_month <- sapply(1:3, function(month) {
+    surviving_hatchery_adults_by_month <- sapply(1:4, function(month) {
       rbinom(31, round(hatchery_by_month[, month]), adult_en_route_surv[, month])
     })
 
