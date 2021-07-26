92e5fccf769dc6cc2460fe9e6fe3b47592a4692f
ErinCain <ecain@flowwest.com>
2021-07-26 16:11:18 -0700

    Replaces fall to spring in egg to fry survival

--- R/surv_egg_to_fry.R
+++ R/surv_egg_to_fry.R
@@ -5,7 +5,7 @@
 #' @param proportion_natural Variable describing the proportion of natural-origin spawners
 #' @param scour Variable describing the probability of redd scouring event
 #' @param temperature_effect Variable describing the effect of inchannel temperature on egg survival.
-#' Fall and spring estimated by C. Hammersmark (CBEC Ecoengineering Inc.). Winter-run value was calibrated.
+#' spring and spring estimated by C. Hammersmark (CBEC Ecoengineering Inc.). Winter-run value was calibrated.
 #' @param ..surv_egg_to_fry_int Intercept
 #' @param .proportion_natural Coefficient for \code{proportion_natural} variable
 #' @param .scour Coefficient for \code{scour} variable
@@ -13,10 +13,10 @@
 #' @export
 surv_egg_to_fry <- function(proportion_natural,
                             scour,
-                            temperature_effect = fallRunDSM::params$mean_egg_temp_effect,
-                            ..surv_egg_to_fry_int = fallRunDSM::params$..surv_egg_to_fry_int,
-                            .proportion_natural = fallRunDSM::params$.surv_egg_to_fry_proportion_natural,
-                            .scour = fallRunDSM::params$.surv_egg_to_fry_scour){
+                            temperature_effect = springRunDSM::params$mean_egg_temp_effect,
+                            ..surv_egg_to_fry_int = springRunDSM::params$..surv_egg_to_fry_int,
+                            .proportion_natural = springRunDSM::params$.surv_egg_to_fry_proportion_natural,
+                            .scour = springRunDSM::params$.surv_egg_to_fry_scour){
 
   boot::inv.logit(..surv_egg_to_fry_int + .proportion_natural * proportion_natural +
                   .scour * scour) * temperature_effect
