725b817d7b2b3a69d19ec8951fa8e253e017995c
ErinCain <ecain@flowwest.com>
2021-07-26 16:08:34 -0700

    Changes fall to spring in ocean entry success

--- R/ocean_entry_success.R
+++ R/ocean_entry_success.R
@@ -10,9 +10,9 @@
 #' @source IP-117068
 #' @export
 ocean_entry_success <- function(migrants, month, avg_ocean_transition_month,
-                                .ocean_entry_success_length = fallRunDSM::params$.ocean_entry_success_length,
-                                ..ocean_entry_success_int = fallRunDSM::params$..ocean_entry_success_int,
-                                .ocean_entry_success_months = fallRunDSM::params$.ocean_entry_success_months){
+                                .ocean_entry_success_length = springRunDSM::params$.ocean_entry_success_length,
+                                ..ocean_entry_success_int = springRunDSM::params$..ocean_entry_success_int,
+                                .ocean_entry_success_months = springRunDSM::params$.ocean_entry_success_months){
 
   month_since <- ifelse(month <= avg_ocean_transition_month, 0, max(1, month - avg_ocean_transition_month))
 
