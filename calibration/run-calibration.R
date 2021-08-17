c07c8f8a42730995ffbfae015d6b67cd4b1c75ea
erodriguez <erodriguez@flowwest.com>
2021-08-17 16:55:00 -0700

    moves reset matrix to year loop and adds sutter fish

--- calibration/run-calibration.R
+++ calibration/run-calibration.R
@@ -39,7 +39,7 @@ readr::write_rds(res, paste0("calibration/fits/result-", Sys.Date(), ".rds"))
 # Evaluate Results ------------------------------------
 
 keep <- c(1,6,7,10,12,19,20,23,26:30)
-r1_solution <- res@solution
+r1_solution <- current_best_solution@solution
 
 r1_params <- update_params(x = r1_solution, fallRunDSM::params)
 r1_params <- DSMCalibrationData::set_synth_years(r1_params)
@@ -81,7 +81,7 @@ r1_eval_df %>%
   xlim(0, 60000) +
   ylim(0, 30000)
 
-r1_eval_df %>%
+x <- r1_eval_df %>%
   spread(type, spawners) %>%
   filter(!is.na(observed)) %>%
   group_by(watershed) %>%
