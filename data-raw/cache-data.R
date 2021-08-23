fae581ec54862ee68ea5f644eea2990379462c46
erodriguez <erodriguez@flowwest.com>
2021-08-23 11:17:46 -0700

    updates diversity groups

--- data-raw/cache-data.R
+++ data-raw/cache-data.R
@@ -83,8 +83,7 @@ usethis::use_data(hatchery_allocation, overwrite = TRUE)
 
 original_groups <- read_csv("data-raw/misc/Grouping.csv")
 
-diversity_group <- original_groups$DiversityGroup
-diversity_group[c(18,19,20,23,26,27)] <- 5
+diversity_group <- original_groups$diversity_group
 names(diversity_group) <- original_groups$watershed
 usethis::use_data(diversity_group, overwrite = TRUE)
 
