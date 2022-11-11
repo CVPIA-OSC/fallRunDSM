e201a1541f021efafb35f1195da881918358b7e9
Emanuel Rodriguez <erodriguez@flowwest.com>
2022-11-11 13:06:58 -0800

    adds the creation of watershed decays from the analysis files to this package

--- data-raw/spawning-decay.R
+++ data-raw/spawning-decay.R
@@ -2,11 +2,81 @@ library(dplyr)
 library(tidyr)
 library(ggplot2)
 library(purrr)
+library(readr)
+library(lubridate)
 
-fallRunDSM::params$spawning_habitat
+# TO CREATE THE WATERSHED DECAY DATASETS ------------------------
+# gravel_size_scaledown <- read_rds("data-raw/gravel-size-scaledowns.rds")
+# rating_curve <- read_rds("data-raw/rating-curves-with-threshold-of-movement.rds")
+# watersheds_with_decay <- read_csv("data-raw/watersheds-with-decays.csv")
+#
+# scaledown_final <- 0.6056206 * .13
+#
+# MIN_flow_cfs_to_sed_cfd_final <- approxfun(
+#   x = rating_curve$flow_cfs,
+#   y = rating_curve$sed_ft3_per_day_min *
+#     gravel_size_scaledown$avg_fraction *
+#     scaledown_final
+# )
+#
+# AVG_flow_cfs_to_sed_cfd_final <- approxfun(
+#   x = rating_curve$flow_cfs,
+#   y = rating_curve$sed_ft3_per_day_avg *
+#     gravel_size_scaledown$avg_fraction *
+#     scaledown_final
+# )
+#
+# MAX_flow_cfs_to_sed_cfd_final <- approxfun(
+#   x = rating_curve$flow_cfs,
+#   y = rating_curve$sed_ft3_per_day_max *
+#     gravel_size_scaledown$avg_fraction *
+#     scaledown_final
+# )
+#
+# dsm_flows <- DSMflow::flows_cfs$biop_2008_2009 |>
+#   # filter(year(date) %in% 1979:2000) |>
+#   pivot_longer(names_to = "watershed", values_to = "flow_cfs", -date) |>
+#   filter(watershed %in% watersheds_with_decay$watershed)
+#
+# # Exceedance probs --------------------------------------
+#
+# exceedance_curves <- map(watersheds_with_decay$watershed, function(w) {
+#   d <- dsm_flows |> filter(watershed == w) |>
+#     mutate(cume_dist = dplyr::cume_dist(-flow_cfs)) |>
+#     arrange(desc(cume_dist))
+#
+#   approxfun(x = d$cume_dist, y = d$flow_cfs)
+# }) |>
+#   set_names(watersheds_with_decay$watershed)
+#
+#
+# upper_sac_exceedance_at_1800 <- 0.0569578
+#
+# watershed_offsets <- map_dbl(watersheds_with_decay$watershed, function(w) {
+#   exceedance_curves[[w]](upper_sac_exceedance_at_1800)
+# }) |> set_names(watersheds_with_decay$watershed)
+#
+# watershed_decays <- map2(watersheds_with_decay$watershed, watershed_offsets, function(w, x) {
+#   dsm_flows |>
+#     filter(watershed == w, year(date) %in% 1979:2000) |>
+#     mutate(flow_adjusted = flow_cfs - x,
+#            decay_min = MIN_flow_cfs_to_sed_cfd_final(flow_adjusted),
+#            decay_avg = AVG_flow_cfs_to_sed_cfd_final(flow_adjusted),
+#            decay_max = MAX_flow_cfs_to_sed_cfd_final(flow_adjusted)
+#     ) |>
+#     pivot_longer(names_to = "decay_type", values_to = "decay_amount", -c(date, watershed,
+#                                                                          flow_cfs, flow_adjusted)) |>
+#     mutate(decay_cfd = ifelse(is.na(decay_amount), 0, decay_amount),
+#            decay_cfm = decay_cfd * days_in_month(month(date)),
+#            decay_sqm = decay_cfm / 2,
+#            decay_acres_month = decay_sqm / 43560) |>
+#     select(date, watershed, flow_cfs, decay_type, decay_acres_month)
+# }) |>
+#   set_names(watersheds_with_decay$watershed)
+#
+# write_rds(watershed_decays, "data-raw/watershed-decay-acres.rds")
 
 watershed_decays <- readr::read_rds("data-raw/watershed-decay-acres.rds")
-watershed_with_decay <- names(watershed_decays)
 
 # spawning_habitat <-
 avg_spawn_hab <- mean(upper_sac_spawn_hab$habitat_acres)
@@ -50,7 +120,7 @@ apply_decay <- function(watershed, spawning_habitat, decay_curve, decay_level = 
   decay
 }
 
-watershed_spawning_hab <- purrr::map_df(watershed_with_decay, function(w) {
+watershed_spawning_hab <- purrr::map_df(watersheds_with_decay$watershed, function(w) {
   fallRunDSM::params$spawning_habitat[w, ,] |>
     as_tibble() |> mutate(month = 1:12) |>
     pivot_longer(`1979`:`2000`, names_to = "year", values_to = "habitat") |>
@@ -66,7 +136,7 @@ watershed_spawning_hab_average <- watershed_spawning_hab |>
   group_by(watershed) |>
   summarise(avg = mean(habitat)) |>
   pull(avg) |>
-  setNames(watershed_with_decay)
+  setNames(watersheds_with_decay$watershed)
 
 
 # For each watershed I need:
@@ -101,7 +171,7 @@ watershed_decays <- readr::read_rds("data-raw/watershed-decay-acres.rds")
 # apply decay to the habitat
 compute_accum_decay <- function(watershed) {
 
-  if (!(watershed %in% watershed_with_decay)) {
+  if (!(watershed %in% watersheds_with_decay$watershed)) {
     watershed_spawning_habitats[[watershed]] |>
       transmute(
         date, decay_type = "none", decayed_habitat_acres = habitat_acres, prop_habitat_decayed = 1
@@ -121,7 +191,10 @@ compute_accum_decay <- function(watershed) {
         prop_habitat_decayed = hab_after_decay_from_mean / habitat_acres
       ) |>
       ungroup() |>
-      select(date, decay_type, decayed_habitat_acres = hab_after_decay_from_mean, prop_habitat_decayed)
+      transmute(date,
+                decay_type = stringr::str_extract(decay_type, "min|avg|max"),
+                decayed_habitat_acres = hab_after_decay_from_mean,
+                prop_habitat_decayed)
   }
 }
 
