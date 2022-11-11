84894527b74752ebdcc92b4f2b1f5863ec99da0a
Emanuel Rodriguez <erodriguez@flowwest.com>
2022-11-11 13:44:33 -0800

    wip: first try and creating a SIT array from the decayed habitat

--- data-raw/spawning-decay.R
+++ data-raw/spawning-decay.R
@@ -8,7 +8,6 @@ library(lubridate)
 # TO CREATE THE WATERSHED DECAY DATASETS ------------------------
 # gravel_size_scaledown <- read_rds("data-raw/gravel-size-scaledowns.rds")
 # rating_curve <- read_rds("data-raw/rating-curves-with-threshold-of-movement.rds")
-# watersheds_with_decay <- read_csv("data-raw/watersheds-with-decays.csv")
 #
 # scaledown_final <- 0.6056206 * .13
 #
@@ -76,49 +75,7 @@ library(lubridate)
 #
 # write_rds(watershed_decays, "data-raw/watershed-decay-acres.rds")
 
-watershed_decays <- readr::read_rds("data-raw/watershed-decay-acres.rds")
-
-# spawning_habitat <-
-avg_spawn_hab <- mean(upper_sac_spawn_hab$habitat_acres)
-
-upper_sac_spawn_hab <- fallRunDSM::params$spawning_habitat["Upper Sacramento River",,] |>
-  as_tibble() |>
-  mutate(month = 1:12) |>
-  pivot_longer(-month, names_to = "year", values_to = "habitat_sqm") |>
-  mutate(habitat_acres = DSMhabitat::square_meters_to_acres(habitat_sqm),
-         date = lubridate::ymd(paste0(year, "-", month, "-", lubridate::days_in_month(month)))) |>
-  select(date, habitat_acres)
-
-first_habitat <- arrange(upper_sac_spawn_hab, date)$habitat_acres[1]
-
-upper_sac_decay <- watershed_decays$`Upper Sacramento River`
-upper_sac_spawning_decayed <- upper_sac_decay |>
-  filter(decay_type == "decay_min") |>
-  left_join(upper_sac_spawn_hab) |>
-  mutate(
-    accum_decay = cumsum(decay_acres_month),
-    diff_from_mean = avg_spawn_hab - accum_decay,
-    diff_from_first = first_habitat - accum_decay,
-    prop_hab_of_mean = habitat_acres / avg_spawn_hab,
-    prop_hab_of_first = habitat_acres / first_habitat,
-    hab_after_decay_from_mean = diff_from_mean * prop_hab_of_mean,
-    hab_after_decay_from_first = diff_from_first * prop_hab_of_first
-  ) |>
-  select(date, hab_after_decay_from_mean )
-
-upper_sac_spawning_decayed |>
-  left_join(upper_sac_spawn_hab) |>
-  mutate(prop_decayed = hab_after_decay_mean / habitat_acres) |>
-  pivot_longer(cols=hab_after_decay_mean:habitat_acres, names_to = "habitat_type", values_to = "habitat",
-               names_transform = \(x) ifelse(x == "hab_after_decay_mean", "decayed", "no decay")) |>
-  filter(lubridate::month(date) %in% 10:12) |>
-  ggplot(aes(date, habitat, fill = habitat_type)) + geom_col(position = "dodge")
-
-
-
-apply_decay <- function(watershed, spawning_habitat, decay_curve, decay_level = c("min", "avg", "max")) {
-  decay
-}
+watersheds_with_decay <- read_csv("data-raw/watersheds-with-decays.csv")
 
 watershed_spawning_hab <- purrr::map_df(watersheds_with_decay$watershed, function(w) {
   fallRunDSM::params$spawning_habitat[w, ,] |>
@@ -132,20 +89,13 @@ watershed_spawning_hab <- purrr::map_df(watersheds_with_decay$watershed, functio
   ) |>
   select(date, watershed, habitat)
 
-watershed_spawning_hab_average <- watershed_spawning_hab |>
-  group_by(watershed) |>
-  summarise(avg = mean(habitat)) |>
-  pull(avg) |>
-  setNames(watersheds_with_decay$watershed)
-
-
 # For each watershed I need:
 # 1. the average spawning habitat
 # 2. time series of habitat
 # 3. time series of decay curves (all three)
 
 # get all watershed averages
-watershed_spawnig_hab_avgs_acres <-
+watershed_spawning_hab_avgs_acres <-
   DSMhabitat::square_meters_to_acres(apply(fallRunDSM::params$spawning_habitat[,10:12,], MARGIN = 1, mean))
 
 
@@ -171,23 +121,23 @@ watershed_decays <- readr::read_rds("data-raw/watershed-decay-acres.rds")
 # apply decay to the habitat
 compute_accum_decay <- function(watershed) {
 
-  if (!(watershed %in% watersheds_with_decay$watershed)) {
-    watershed_spawning_habitats[[watershed]] |>
-      transmute(
-        date, decay_type = "none", decayed_habitat_acres = habitat_acres, prop_habitat_decayed = 1
-      )
-  } else {
-    watershed_decays[[watershed]] |>
+  base_ts <- watershed_spawning_habitats[[watershed]] |>
+    transmute(
+      date, decay_type = "none", decayed_habitat_acres = habitat_acres, prop_habitat_decayed = 1
+    )
+
+  if (watershed %in% watersheds_with_decay$watershed) {
+
+    avg_spawn_hab <- watershed_spawning_hab_avgs_acres[watershed]
+
+    decay <- watershed_decays[[watershed]] |>
       left_join(watershed_spawning_habitats[[watershed]]) |>
       group_by(decay_type) |>
       mutate(
         accum_decay = cumsum(decay_acres_month),
         diff_from_mean = avg_spawn_hab - accum_decay,
-        diff_from_first = first_habitat - accum_decay,
         prop_hab_of_mean = habitat_acres / avg_spawn_hab,
-        prop_hab_of_first = habitat_acres / first_habitat,
         hab_after_decay_from_mean = diff_from_mean * prop_hab_of_mean,
-        hab_after_decay_from_first = diff_from_first * prop_hab_of_first,
         prop_habitat_decayed = hab_after_decay_from_mean / habitat_acres
       ) |>
       ungroup() |>
@@ -195,12 +145,49 @@ compute_accum_decay <- function(watershed) {
                 decay_type = stringr::str_extract(decay_type, "min|avg|max"),
                 decayed_habitat_acres = hab_after_decay_from_mean,
                 prop_habitat_decayed)
+
+    base_ts <- bind_rows(base_ts, decay)
+
   }
+
+  return(base_ts)
 }
 
 decayed_habitats <- map(fallRunDSM::watershed_labels, \(w) compute_accum_decay(w)) |>
   set_names(fallRunDSM::watershed_labels)
-decayed_habitats$`Upper Sacramento River`
+
+watershed_decay_all_none <- rep("none", 31)
+watershed_decay_all_none <- setNames(watershed_decay_all_none, fallRunDSM::watershed_labels)
+
+decay_str_to_array <- function(watershed_decay_levels) {
+  d <- map(fallRunDSM::watershed_labels, function(w) {
+    decayed_habitats[[w]] |>
+      filter(decay_type == watershed_decay_all_none[w]) |>
+      mutate(watershed = w) |>
+      mutate(month = month(date), year = year(date)) |>
+      select(watershed, month, year, decayed_habitat_acres) |>
+      pivot_wider(names_from = year, values_from = decayed_habitat_acres) |>
+      select(-watershed, -month) |>
+      as.matrix()
+  })
+
+  a <- array(NA_real_, dim = c(31, 12, 22), dimnames = list(fallRunDSM::watershed_labels,
+                                                             month.abb,
+                                                             1979:2000))
+
+  for (i in 1:31) {
+    a[i,,] <- d[[i]]
+  }
+
+  return(a)
+}
+
+watershed_decay_all_none["Upper Sacramento River"] <- "min"
+x <- decay_str_to_array(watershed_decay_all_none)
+x["Upper Sacramento River",,]
+
+
+
 
 
 
